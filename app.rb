1114dfbdbbe8fe6525e244afb90df2b675ea4a45
Br0ckers <neil.brockman@yahoo.co.uk>
2018-10-15 15:44:25 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,16 @@
+#app.rb
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
+
+get '/secret' do
+ 'This is a secret page really!!'
+end
+
+
+
