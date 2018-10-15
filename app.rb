6758dc87f361fa7a1d1501661717bcc20897f92a
Br0ckers <neil.brockman@yahoo.co.uk>
2018-10-15 17:06:56 +0100

    Start using forms

--- app.rb
+++ app.rb
@@ -2,13 +2,19 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
-get '/cat' do
+get '/random-cat' do
  @name = ["Amigo", "Oscar", "Viking"].sample 
   erb(:index)
 end
 
 get '/secret' do
  'This is a secret page really!!'
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
 end
 
 
