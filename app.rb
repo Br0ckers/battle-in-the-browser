01021470b23d4d891f140d6e63038e279c27a189
Br0ckers <neil.brockman@yahoo.co.uk>
2018-10-15 15:50:48 +0100

    second commit

--- app.rb
+++ app.rb
@@ -3,9 +3,7 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb(:index)
 end
 
 get '/secret' do
